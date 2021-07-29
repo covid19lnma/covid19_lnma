9e560efc7045f9847f6e0dbd968363b51579f453
Antonio-Kuri <al.kuri@ciencias.unam.mx>
2021-07-29 03:11:59 -0500

    Bayesian MA blood severe.R re-routed to covid19_lnma directory for uniformity with the NMA file

--- pairwise/Bayesian MA blood severe.R
+++ pairwise/Bayesian MA blood severe.R
@@ -1,10 +1,20 @@
-#wd <- "/home/antonio/covid19_lnma/pairwise"
+#wd <- "/home/antonio/covid19_lnma"
 #setwd(wd)
-source("functions_MA.R")
+source("pairwise/functions_MA.R")
+
+mainDir <- paste0(getwd(),"/pairwise/blood")
+subDir <- "output"
+
+output_dir <- file.path(mainDir, subDir)
+
+if (!dir.exists(output_dir)){
+  dir.create(output_dir)
+}
+
 
 ########### Ventilation free days ####
 
-data=read.csv("blood/severe/Ventilator-free days_wide data.csv")
+data=read.csv("pairwise/blood/severe/Ventilator-free days_wide data.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -17,21 +27,15 @@ baseline=data %>% filter(t1=="placebo/standard care" | t2=="placebo/standard car
 
 measure <- "MD"
 name <- "vent_free_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 ########### Time to symptom resolution ####
 
-data=read.csv("blood/severe/Time to symptom resolution_wide data.csv")
+data=read.csv("pairwise/blood/severe/Time to symptom resolution_wide data.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -44,21 +48,15 @@ baseline=data %>% filter(t1=="placebo/standard care" | t2=="placebo/standard car
 
 measure <- "ROM"
 name <- "symptom_resolution_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 ########### ICU length of stay ####
 
-data=read.csv("blood/severe/ICU length of stay_wide data.csv")
+data=read.csv("pairwise/blood/severe/ICU length of stay_wide data.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -71,21 +69,15 @@ baseline=data %>% filter(t1=="placebo/standard care" | t2=="placebo/standard car
 
 measure <- "MD"
 name <- "ICU_length_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 ########### Duration of hospitalization ####
 
-data=read.csv("blood/severe/Duration of hospitalization_wide data.csv")
+data=read.csv("pairwise/blood/severe/Duration of hospitalization_wide data.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -98,22 +90,16 @@ baseline=data %>% filter(t1=="placebo/standard care" | t2=="placebo/standard car
 
 measure <- "MD"
 name <- "duration_hosp_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################transfusion-related acute lung injury ###########################
 
 
-data=read.csv("blood/severe/transfusion-related acute lung injury - wide data format.csv")
+data=read.csv("pairwise/blood/severe/transfusion-related acute lung injury - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -128,22 +114,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "acute_lung_injury_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################transfusion-associated circulatory overload ###########################
 
 
-data=read.csv("blood/severe/transfusion-associated circulatory overload - wide data format.csv")
+data=read.csv("pairwise/blood/severe/transfusion-associated circulatory overload - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -158,22 +138,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "circulatory_overload_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################Mortality ###########################
 
 
-data=read.csv("blood/severe/mortality - wide data format.csv")
+data=read.csv("pairwise/blood/severe/mortality - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -188,22 +162,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "mortality_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################Mechanical Ventilation ###########################
 
 
-data=read.csv("blood/severe/mechanical ventilation - wide data format.csv")
+data=read.csv("pairwise/blood/severe/mechanical ventilation - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -218,21 +186,15 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "mv_severe.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 ########### Time to viral cle ####
 
-data=read.csv("blood/not severe/Time to viral clearance_wide data.csv")
+data=read.csv("pairwise/blood/not severe/Time to viral clearance_wide data.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -245,21 +207,15 @@ baseline=data %>% filter(t1=="placebo/standard care" | t2=="placebo/standard car
 
 measure <- "ROM"
 name <- "time_viral_clear_notsevere.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 ########### Time to symptom resolution ####
 
-data=read.csv("blood/not severe/Time to symptom resolution_wide data.csv")
+data=read.csv("pairwise/blood/not severe/Time to symptom resolution_wide data.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -272,21 +228,15 @@ baseline=data %>% filter(t1=="placebo/standard care" | t2=="placebo/standard car
 
 measure <- "ROM"
 name <- "symptom_resolution_notsevere.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 ########### Duration of hospitalization ####
 
-data=read.csv("blood/not severe/Duration of hospitalization_wide data.csv")
+data=read.csv("pairwise/blood/not severe/Duration of hospitalization_wide data.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -299,22 +249,16 @@ baseline=data %>% filter(t1=="placebo/standard care" | t2=="placebo/standard car
 
 measure <- "MD"
 name <- "duration_hosp_notsevere.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################Viral Clear ###########################
 
 
-data=read.csv("blood/not severe/viral clearance - wide data format.csv")
+data=read.csv("pairwise/blood/not severe/viral clearance - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -329,22 +273,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "viral_clear_notsevere.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################Mortality ###########################
 
 
-data=read.csv("blood/not severe/mortality - wide data format.csv")
+data=read.csv("pairwise/blood/not severe/mortality - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -359,22 +297,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "mortality_notsevere.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name, folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################Mechanical Ventilation ###########################
 
 
-data=read.csv("blood/not severe/mechanical ventilation - wide data format.csv")
+data=read.csv("pairwise/blood/not severe/mechanical ventilation - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -389,22 +321,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "mv_notsevere.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################admission to hosp ###########################
 
 
-data=read.csv("blood/not severe/admission to hospital - wide data format.csv")
+data=read.csv("pairwise/blood/not severe/admission to hospital - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -419,22 +345,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "admission_hosp_notsevere.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################Allergic ###########################
 
 
-data=read.csv("blood/allergic reactions - wide data format.csv")
+data=read.csv("pairwise/blood/allergic reactions - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -449,22 +369,16 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "allergic_reac.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
 #######################AE ###########################
 
 
-data=read.csv("blood/adverse effects leading to discontinuation - wide data format.csv")
+data=read.csv("pairwise/blood/adverse effects leading to discontinuation - wide data format.csv")
 data=data %>% mutate(t1=gsub("^\\d+_(.*$)","\\1",t1),t2=gsub("^\\d+_(.*$)","\\1",t2)) 
 
 # determine corresponding prior parameters(?TurnerEtAlPrior to help):
@@ -479,15 +393,9 @@ baseline=data %>%
 
 measure <- "OR"
 name <- "AEs.csv"
-folder <- "blood"
 
-output_dir <- file.path(folder, "output")
 
-if (!dir.exists(output_dir)){
-  dir.create(output_dir)
-}
+list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,mainDir)
 
-list.estimates <- getestimates(data, TP, TP1, baseline, measure, name,folder)
-
-write.estimates.csv(list.estimates,folder, name)
+write.estimates.csv(list.estimates,mainDir, name)
 
